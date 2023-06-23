91fdddf048939e13646103fbe4bbc360bb704a09
AGUSTINA YAEL ANGEL <46645633@ort>
2023-06-23 12:08:17 -0300

    varios

--- src/components/IniciarSesion.js
+++ src/components/IniciarSesion.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
+import { donarContext } from '../donarContext.js';
 
 function IniciarSesion({ setMostrarIniciarSesion }) {
-
+  //const usuarioC = useContext(donarContext);
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   //CREO EL OBJETO Y SE LO PASO A NAVBAR
 
-  const onSubmit = async(data) => {
+  const onSubmit = async (data) => {
     console.log(data);
 
     if (data.mailBuscar == '' || data.contraseñaBuscar == '') {
@@ -16,24 +18,38 @@ function IniciarSesion({ setMostrarIniciarSesion }) {
 
     }
     else {
-      /*try{
-        const response = await axios.post(process.env.REACT_APP_BASE_URL_DONAR);
-        console.log(response);
+      try {
+        const response = await axios.post(process.env.REACT_APP_BASE_URL_DONAR + "/auth/login", data);
 
-        //Validar si la response está ok (status code === 200)
-        if(response.ok){
-          //Login correcto...
-
+        if (response.status === 200) {
           const data = await response.data;
           console.log(data.access_token);
-          console.log(data.user);
+          console.log(data.usuario);
+          console.log(data.tipo);
+
+          <donarContext.Provider value={
+            {
+              access_token: data.access_token,
+              usuario: data.usuario,
+              tipo: data.tipo
+
+            }}/>
+
+
+          //console.log(usuarioC);
+
+          //aca guardo con el context el objeto. No tengo que escribir los atributos sino guardarlo de forma global
+          //if (tipo=doante){}
+          //if (tipo=ONG){}
+
+
 
         } else {
-          throw new Error(await response.data);
+          throw new Error("Logueo incorrecto");
         }
       } catch (error) {
         console.log(error);
-      }*/
+      }
 
       //Por ahora guardo el token y el usuario harcodeado
       const token = "abc123";
@@ -42,9 +58,9 @@ function IniciarSesion({ setMostrarIniciarSesion }) {
         apellido: "Pepe",
         tipo: "ONG",
         direccion: "Yatay 240",
-        
+
       }
-      
+
     }
   }
 
@@ -59,8 +75,8 @@ function IniciarSesion({ setMostrarIniciarSesion }) {
             <h5 className="card-title tituloCard pb-5">Iniciar Sesión</h5>
 
             <form onSubmit={handleSubmit(onSubmit)}>
-              <input type="text" {...register("mailBuscar")} className="u-full-width medidainput" placeholder="Mail" autoComplete="off" />
-              <input type="text" {...register("constraseñaBuscar")} className="u-full-width medidainput" placeholder="Contraseña" autoComplete="off" />
+              <input type="text" {...register("mail")} className="u-full-width medidainput" placeholder="Mail" autoComplete="off" />
+              <input type="password" {...register("password")} className="u-full-width medidainput" placeholder="Contraseña" autoComplete="off" />
               <a className="boton nav-link px-5 colorLetraIni pb-5" href="#">
                 ¿Has olvidado tu contraseña?
               </a>
