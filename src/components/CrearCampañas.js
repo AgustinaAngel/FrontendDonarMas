88d4cdcbd3f66c01d009c5b807a3b20342f4f75d
unknown <46645633@comunidadort.edu.ar>
2023-07-05 20:15:56 -0300

    Crear campaña base

--- src/components/CrearCampañas.js
+++ src/components/CrearCampañas.js
@@ -1,86 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useForm } from "react-hook-form";
-import axios from 'axios';
-import { useContext } from 'react';
-import user from '../context/user.js';
-import navBarLink from '../context/navBarLink.js';
-//import CrearCampañas from './CrearCampañas';
+import React, { useState } from 'react';
 
+const CrearCampañas = () => {
+  const [showForm, setShowForm] = useState(false);
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
 
-function CrearCampañas() {
+  const handleClick = () => {
+    setShowForm(true);
+  };
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  
-  const onSubmit = async (data) => {
-    console.log(data);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para manejar los datos del formulario
+    console.log('Nombre:', nombre);
+    console.log('Descripción:', descripcion);
+  };
 
-    if (data.mailBuscar === '' || data.contraseñaBuscar === '') {
-      console.log("debo mostar error");
-
-    }
-    else {
-      try {
-        const response = await axios.post(process.env.REACT_APP_BASE_URL_DONAR + "/campanias", data);
-
-        if (response.status === 200) {
-        
-
-          console.log("se creo con exito");
-      
-
-        } else {
-          throw new Error("Hubo un error");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-
-
-     
-
-
-    }
-
-  }
-
-
-return (
-  <>
-
-    <div className="pb-5"></div>
-    <div className="pb-5"></div>
-    <div className="pb-5"></div>
-    <center>
-      <div className="card text-center cardIni">
-        <div className="card-body">
-          <h5 className="card-title tituloCard pb-5">Iniciar Sesión</h5>
-
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" {...register("mail")} className="u-full-width medidainput" placeholder="Mail" autoComplete="off" />
-            <input type="password" {...register("password")} className="u-full-width medidainput" placeholder="Contraseña" autoComplete="off" />
-            <a className="boton nav-link px-5 colorLetraIni pb-5" href="#">
-              ¿Has olvidado tu contraseña?
-            </a>
-            <button type="submit" className="botonVioleta">Iniciar Sesión</button>
-          </form>
-
-        </div>
-      </div>
-      <div className="text-muted">
-        <Link className="boton nav-link px-5 colorLetraIni" to="/registro">
-          ¿No tienes un perfil? ¡Regístrate!
-        </Link>
-      </div>
-    </center>
-
-
-
-
-
-  </>
-)}
-
-
+  return (
+    <div>
+      <button onClick={handleClick}>Mostrar formulario</button>
+      {showForm && (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nombre:
+            <input
+              type="text"
+              name="nombre"
+              value={nombre}
+              onChange={(event) => setNombre(event.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Descripción:
+            <textarea
+              name="descripcion"
+              value={descripcion}
+              onChange={(event) => setDescripcion(event.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">Enviar</button>
+        </form>
+      )}
+    </div>
+  );
+};
 
 export default CrearCampañas;
